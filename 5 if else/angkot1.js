efb45a10315e2bbaeb21515f9de247dfba4dc896
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-28 16:07:08 +0700

    belajar else if

---
+++ 5 if else/angkot1.js
@@ -0,0 +1,14 @@
+var jumlahAngkot = 10;
+var angkotSehat = 6;
+
+for (noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++)
+{
+    if (noAngkot <= angkotSehat)
+    {
+        console.log('angkot No.' + noAngkot + ' beroperasi dengan baik')
+    }
+    else
+    {
+        console.log('angkot No.' + noAngkot + ' tidak beroperasi dengan baik');
+    }
+}

6c316beab2180d5f94278273d90b478869d777df
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-29 09:16:29 +0700

    membuat game tebak angka

---
+++ 10 game tebak angka/style.js
@@ -0,0 +1,30 @@
+let play = true;
+const kesempatan = 3;
+alert('Tebak Angka dari 1 - 10\nKamu punya ' + kesempatan + ' kali kesempatan');
+
+while (play) {
+  const com = Math.floor(Math.random() * 11);
+  let i = kesempatan;
+  for (i; i > 0; i--) {
+    const p = prompt('Kesempatan ke- ' + i);
+    let sisaKesempatan = i - 1;
+    if (p == com) {
+      alert('Angka yg kamu masukkan : ' + p + '\nSelamat Kamu Benar');
+      break;
+    } else if (p < com) {
+      if (sisaKesempatan == 0) {
+        alert('Gagal.. \nNomor yang dicari adalah '+com);
+      } else {
+        alert('Terlalu Rendah...\nAyo kamu masih punya ' + sisaKesempatan + ' Kesempatan!');
+      }
+    } else if (p > com) {
+      if (sisaKesempatan == 0) {
+        alert('gagal');
+      } else {
+        alert('Terlalu Tinggi...\nAyo kamu masih punya ' + sisaKesempatan + ' kesempatan!');
+      }
+    }
+  }
+
+  play = confirm('Main Lagi?');
+}

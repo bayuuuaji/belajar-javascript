912ad15a0f0018fa84b24fc6df657e10a6d81ed9
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-29 18:06:41 +0700

    memberi keterangan pada js

--- 10 game tebak angka/style.js
+++ 10 game tebak angka/style.js
@@ -1,19 +1,23 @@
 let play = true;
 const kesempatan = 3;
+// memasukkan nomor user
 alert('Tebak Angka dari 1 - 10\nKamu punya ' + kesempatan + ' kali kesempatan');
 
 while (play) {
+  // mencari nomor random computer
   const com = Math.floor(Math.random() * 11);
   let i = kesempatan;
   for (i; i > 0; i--) {
     const p = prompt('Kesempatan ke- ' + i);
     let sisaKesempatan = i - 1;
+
+    //   rules
     if (p == com) {
       alert('Angka yg kamu masukkan : ' + p + '\nSelamat Kamu Benar');
       break;
     } else if (p < com) {
       if (sisaKesempatan == 0) {
-        alert('Gagal.. \nNomor yang dicari adalah '+com);
+        alert('Gagal.. \nNomor yang dicari adalah ' + com);
       } else {
         alert('Terlalu Rendah...\nAyo kamu masih punya ' + sisaKesempatan + ' Kesempatan!');
       }
@@ -28,3 +32,4 @@ while (play) {
 
   play = confirm('Main Lagi?');
 }
+alert('Terimakasih Sudah Bermain!');

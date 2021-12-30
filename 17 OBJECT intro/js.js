2655fd8f2a63893e953548118b65cd7c10fab086
bayuuuaji <bayuuuajisantoso@gmail.com>
2021-12-30 16:36:22 +0700

    17 OBJECT intro

---
+++ 17 OBJECT intro/js.js
@@ -0,0 +1,19 @@
+// membuaj object
+var mhs = {
+    nama: 'bayu',
+    umur : 19,
+    ips: [3.00, 2.59, 2.84, 3.6],
+    alamat: {
+        jalan: 'JL. ABC',
+        no: 50,
+        kota: 'Jombang',
+        provinsi : 'Jawa Timur'
+        
+    }
+};
+
+// cara memanggil
+// mhs.nama
+// mhs["nama"]
+// mhs.ips[2]
+// mhs.alamat.jalan
